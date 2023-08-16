998236d3d8824f64cb2cd8d035fd07233de58667
Ivan Lesnykh <ivan.lesnykh.official@gmail.com>
2023-08-16 16:30:41 +0300

    Initial Spring Boot commit.

---
+++ src/main/java/com/mailingtracker/MailingTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.mailingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailingTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mailingtracker/MailingTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mailingtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailingTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
